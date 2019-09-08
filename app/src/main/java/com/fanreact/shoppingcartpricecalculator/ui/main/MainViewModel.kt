d1b36086835ea5459d3c899e188f66e07224f4cc
avinash_david <adavid@fanreact.com>
2019-09-08 17:31:58 -0500

    Initial commit

---
+++ app/src/main/java/com/fanreact/shoppingcartpricecalculator/ui/main/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.fanreact.shoppingcartpricecalculator.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.fanreact.shoppingcartpricecalculator.product.Product
+import com.fanreact.shoppingcartpricecalculator.purchase.Purchase
+
+class MainViewModel : ViewModel() {
+
+}
