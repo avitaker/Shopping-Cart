0bd11b547a8fe9b731dc2ce4b60a588382fcf08d
avinash_david <adavid@fanreact.com>
2019-09-08 19:29:01 -0500

    Added viewmodel for use in fragment

--- app/src/main/java/com/fanreact/shoppingcartpricecalculator/ui/main/MainViewModel.kt
+++ app/src/main/java/com/fanreact/shoppingcartpricecalculator/ui/main/MainViewModel.kt
@@ -1,11 +1,61 @@
 package com.fanreact.shoppingcartpricecalculator.ui.main
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.fanreact.shoppingcartpricecalculator.product.Product
 import com.fanreact.shoppingcartpricecalculator.purchase.Purchase
+import com.fanreact.shoppingcartpricecalculator.utilities.DataConversionUtils
 
 class MainViewModel : ViewModel() {
+    private var purchase : Purchase = Purchase()
+    set(value) {
+        field = value
+        purchaseMutableLiveData.postValue(value)
+    }
 
+    private val purchaseMutableLiveData = MutableLiveData<Purchase>()
+    val purchaseLiveData: LiveData<Purchase> get() = purchaseMutableLiveData
+
+    val productsLiveData = MediatorLiveData<List<Product>>()
+    val salesTaxLiveData = MediatorLiveData<String>()
+    val totalPriceLiveData = MediatorLiveData<String>()
+
+    init {
+        productsLiveData.addSource(purchaseMutableLiveData) { purchase ->
+            productsLiveData.postValue(purchase.products())
+        }
+        salesTaxLiveData.addSource(purchaseMutableLiveData) { purchase ->
+            salesTaxLiveData.postValue(DataConversionUtils.priceDisplayString(purchase.totalSalesTax))
+        }
+        totalPriceLiveData.addSource(purchaseMutableLiveData) { purchase ->
+            salesTaxLiveData.postValue(DataConversionUtils.priceDisplayString(purchase.totalPriceWithTax))
+        }
+    }
+
+    fun addProducts(listOfProducts: List<Product>) {
+        purchase = purchase.apply {
+            addProducts(listOfProducts)
+        }
+    }
+
+    fun addProduct(product: Product) {
+        purchase = purchase.apply {
+            addProduct(product)
+        }
+    }
+
+    fun removeProduct(product: Product) {
+        purchase = purchase.apply {
+            removeProduct(product)
+        }
+    }
+
+    fun clearProducts() {
+        purchase = purchase.apply {
+            clearProducts()
+        }
+    }
 }
