e8410ce517ed94f7ebb30701249f73b0cf845cf8
avinash_david <adavid@fanreact.com>
2019-09-08 20:23:01 -0500

    Added some brief UI

---
+++ app/src/main/java/com/fanreact/shoppingcartpricecalculator/ui/main/ShoppingCartProductRecyclerView.kt
@@ -0,0 +1,71 @@
+package com.fanreact.shoppingcartpricecalculator.ui.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.fanreact.shoppingcartpricecalculator.R
+import com.fanreact.shoppingcartpricecalculator.product.Product
+import com.fanreact.shoppingcartpricecalculator.utilities.DataConversionUtils
+import kotlinx.android.synthetic.main.item_product.view.*
+
+class ShoppingCartProductAdapter(private val context: Context) : RecyclerView.Adapter<ShoppingCartProductAdapter.ProductViewHolder>(){
+    private val products = mutableListOf<Product>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder(LayoutInflater.from(context).inflate(R.layout.item_product, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val productPair = products[position]
+
+        holder.apply {
+            tvName.text = productPair.name
+            tvCost.text = DataConversionUtils.priceDisplayString(productPair.baseCost)
+//            tvCount.text = "1"
+        }
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    fun setProducts(products: List<Product>) {
+        this.products.apply {
+            clear()
+            addAll(products)
+            notifyDataSetChanged()
+        }
+    }
+
+    fun addProduct(product: Product, position: Int = products.count()) {
+        val translatedPosition = if (position !in 0 until products.count()) {
+            products.count()
+        } else {
+            position
+        }
+        this.products.apply {
+            add(translatedPosition, product)
+            notifyItemInserted(translatedPosition)
+        }
+    }
+
+    fun removeProduct(id: String) {
+        products.firstOrNull { p -> p.id == id }?.let {
+            val index = products.indexOf(it)
+            products.removeAt(index)
+            notifyItemRemoved(index)
+        }
+    }
+
+    class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvName = itemView.tvName
+        val tvCost = itemView.tvCost
+        val tvCount = itemView.tvCount
+    }
+}
